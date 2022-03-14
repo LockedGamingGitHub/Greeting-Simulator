e1b156efe50cbd63e559d031aa7518309b7c3304
4444dogs <lee@juszak.net>
2022-03-14 16:07:58 -0600

    Remove Unnecessary Imports and Add Tags

--- Greeting Simulator ULTRA.py
+++ Greeting Simulator ULTRA.py
@@ -1,11 +1,12 @@
-from glob import glob
-from multiprocessing.connection import wait
-from re import U
+# Imports
 from tkinter import *
 from random import randint
 
+# Random Variables
 count = 0
+window = Tk()
 
+# What Happens When You Click the Greet Button
 def click():
     global count
     goodluck = randint(1, 50)
@@ -23,7 +24,7 @@ def click():
 
     
     
-window = Tk()
+# Configuring the Greet Button
 button = Button(window, text='Greet Someone Like a Gamer!')
 button.config(command=click)
 button.config(font=('Open Sans', 50, 'bold'))
@@ -31,10 +32,12 @@ button.config(bg='#b8860b')
 button.config(fg='#000000')
 button.config(activebackground='#daa520')
 button.config(activeforeground='#000000')
+# Configuring the Greet Counter
 label = Label(window, text="Start greeting people!", fg= "blue")
 label.config(font=('Monospace', 40, 'bold'))
 label.pack()
 button.pack()
+# Counfiguring the Window
 window.title('Greeting Simulator ULTRA')
 window.state("zoomed")
 window.mainloop()
